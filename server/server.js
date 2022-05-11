962017cf3d1617b8d03e9bfac65c09cb6c2752e0
mabus <m.buscaroli@outlook.com>
2022-05-11 10:45:08 +0100

    setup two GET routes that return fake data or no data

--- server/server.js
+++ server/server.js
@@ -10,3 +10,20 @@ module.exports = app
 
 // We are going to save the links from google in this array
 let dataArray = []
+
+app.get('/links', (req, res) => {
+  res.send(dataArray)
+})
+
+app.get('/links/random', (req, res) => {
+  const isArrayEmpty = dataArray.length === 0 ? true : false
+  console.log(isArrayEmpty)
+
+  if (isArrayEmpty) {
+    res
+      .status(404)
+      .send({ message: 'No links to show, try coming back later...' })
+  } else {
+    res.send(dataArray)
+  }
+})
