811afc2d960f29f9a4dd065f9e86b53a1218771f
mabus <m.buscaroli@outlook.com>
2022-05-12 19:27:45 +0100

    cleaned code both frontend and backend, /GET links route wasnt needed as it is sufficient to get the dta back from the post request, this sorts also the error where every search would return the results related to the previous input

--- client/script.js
+++ client/script.js
@@ -5,16 +5,15 @@ const form = document.querySelector('.search-btns')
 const content = document.querySelector('.content-section')
 const luckyBtn = document.querySelector('.lucky-btn')
 
-let savedResults = []
+let savedMovies = []
 
 searchInput.addEventListener('keydown', function (event) {
   resultSection.innerHTML = ''
 
   if (event.code === 'Enter' && searchInput.value !== '') {
-    // search()
     const input = searchInput.value
-    console.log('input - ', input)
-    sendSearchTerm(input)
+
+    let returnedSearch = sendSearchTerm(input)
   }
 })
 
@@ -22,27 +21,29 @@ searchButton.addEventListener('click', function (event) {
   event.preventDefault()
   if (searchInput.value !== '') {
     const input = searchInput.value
-    console.log('input - ', input)
-    sendSearchTerm(input)
+
+    let returnedSearch = sendSearchTerm(input)
   }
 })
 
 luckyBtn.addEventListener('click', getRandom)
 
 function getRandom() {
-  if (savedResults.length === 0) {
+  let moviesWithLink = savedMovies.filter((movie) => movie.Poster !== 'N/A')
+
+  if (moviesWithLink.length === 0) {
     window.open('https://http.cat/405')
   } else {
-    let randomMovieIndex = Math.floor(Math.random() * savedResults.length)
-    console.log('lucky -> ', savedResults[randomMovieIndex].Poster)
-    window.open(savedResults[randomMovieIndex].Poster)
+    let randomMovieIndex = Math.floor(Math.random() * moviesWithLink.length)
+
+    window.open(moviesWithLink[randomMovieIndex].Poster)
   }
-  let randomMovieIndex = Math.floor(Math.random() * savedResults.length)
-  console.log('lucky -> ', savedResults[randomMovieIndex].Poster)
-  window.open(savedResults[randomMovieIndex].Poster)
+  let randomMovieIndex = Math.floor(Math.random() * moviesWithLink.length)
+
+  window.open(moviesWithLink[randomMovieIndex].Poster)
 }
 
-// sends a POSt request to our server
+// sends a POST request to our server
 function sendSearchTerm(term) {
   fetch('http://localhost:3000/links', {
     method: 'POST',
@@ -52,36 +53,11 @@ function sendSearchTerm(term) {
       mode: 'no-cors',
     },
     body: JSON.stringify({ term: term }),
-  }).then((response) => {
-    console.log(response)
-    getLinks()
-  })
-}
-
-// this function gets the movies from our dataArray storen in our server:
-// Basically when calling this function we are making a api request to
-// our server to GET /links
-function getLinks() {
-  fetch('http://localhost:3000/links', {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      mode: 'no-cors',
-    },
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log('script.js, getLinks() -> ', data)
-      // window.location.reload()
-
-      savedResults = data
-
-      let newArray = savedResults.filter((movie) => movie.Poster !== 'N/A')
-      savedResults = newArray
-      console.log('movies with links -> ', savedResults)
-
-      createCards(data)
+      savedMovies = data
+      createCards(savedMovies)
     })
 }
 
@@ -92,18 +68,12 @@ function createCards(data) {
 }
 
 function createAndAppendCard(movie) {
-  console.log(movie)
-
   const card = document.createElement('div')
   card.classList.add('movieCard')
 
   const titleText = movie.Title
   const linkText = movie.Poster
   const yearText = movie.Year
-
-  // console.log('title: ', title)
-  // console.log('link ', link)
-  // console.log(year)
 
   const titleElement = document.createElement('h3')
   titleElement.textContent = titleText
@@ -113,7 +83,7 @@ function createAndAppendCard(movie) {
 
   const linkElement = document.createElement('a')
   linkElement.href = linkText
-  linkElement.textContent = 'Visit Page' // TODO value or textContent ?!?
+  linkElement.textContent = 'Visit Page'
   linkElement.classList.add('card__link')
   linkElement.target = '_blank'
 
@@ -126,5 +96,6 @@ function createAndAppendCard(movie) {
 
   resultSection.append(card)
 
+  // clear the searchbar after the input has been captured (click or Enter)
   searchInput.value = ''
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { restart } = require('nodemon')
+const { ConsoleWriter } = require('istanbul-lib-report')
 const axios = require('axios').default
 
 const app = express()
@@ -13,13 +14,14 @@ module.exports = app
 // We are going to save the movies in this array
 let dataArray = []
 
-app.get('/links', (req, res) => {
-  if (dataArray.length === 0) {
-    res.status(404)
-  } else {
-    res.send(dataArray)
-  }
-})
+// app.get('/links', (req, res) => {
+//   if (dataArray.length === 0) {
+//     res.status(404)
+//   } else {
+//     console.log('server.js, GET /links, dataArray before sending:', dataArray)
+//     res.send(dataArray)
+//   }
+// })
 
 // app.get('/links/random', async (req, res) => {
 //   const isArrayEmpty = dataArray.length === 0 ? true : false
@@ -40,105 +42,33 @@ app.post('/links', (req, res) => {
   let term = req.body.term
 
   console.log('server.js POST links', term)
-  // getData(term.term)
 
-  // console.log(typeof term.term)
-
-  // we call the getData function
-  getData(term)
-
-  res.send()
+  let response = getData(term)
+  // console.log('server.js post /links response', response)
+  response.then((data) => {
+    // console.log('server.js POST /links - data -> ', data)
+    dataArray = data
+    // console.log('server.js POST /links AAAAAA dataArray -> ', dataArray)
+    console.log('JSON.stringify ->', JSON.stringify(data))
+    res.send(JSON.stringify(data))
+  })
 })
 
 // it fetches the movies from the omdb api
-function getData(searchTerm = 'nature') {
-  axios('https://www.omdbapi.com/?', {
+async function getData(searchTerm = 'nature') {
+  let movieArray = axios('https://www.omdbapi.com/?', {
     params: {
       s: searchTerm,
       apikey: '55c9aadf',
     },
   }).then((response) => {
     console.log('==========', response.data.Search)
-
-    // it saves the movies from the api into our database array
-    dataArray = response.data.Search
+    console.log(
+      'server.js - getData -> type of response.data.Search',
+      typeof response.data.Search
+    )
+    // I have a
+    return response.data.Search
   })
+  return movieArray
 }
-
-function getRandom() {
-  let randomIndex = Math.floor(Math.random() * dataArray.length)
-
-  let randomAddressObject = dataArray[randomIndex]
-  return randomAddressObject
-}
-
-//  {
-//     Title: 'Ace Ventura: When Nature Calls',
-//     Year: '1995',
-//     imdbID: 'tt0112281',
-//     Type: 'movie',
-//     Poster: 'https://m.media-amazon.com/images/M/MV5BNGFiYTgxZDctNGI4OS00MWU1LWIwOGUtZmMyNGQxYjVkZjQ3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg'
-//   },
-//   {
-//     Title: 'Forces of Nature',
-//     Year: '1999',
-//     imdbID: 'tt0141098',
-//     Type: 'movie',
-//     Poster: 'https://m.media-amazon.com/images/M/MV5BNWI5OWE0MWQtNjliMi00MjE4LTg5ODAtOTM0YjA2OWVlYzgwXkEyXkFqcGdeQXVyNjU0NTI0Nw@@._V1_SX300.jpg'
-//   },
-//   {
-//     Title: 'Human Nature',
-//     Year: '2001',
-//     imdbID: 'tt0219822',
-//     Type: 'movie',
-//     Poster: 'https://m.media-amazon.com/images/M/MV5BM2FiNzM1NzgtYzJmYi00NzRjLWFmYzktMzI1M2FlMWYzYjVhXkEyXkFqcGdeQXVyNTM0NTU5Mg@@._V1_SX300.jpg'
-//   },
-//   {
-//     Title: 'Freaks of Nature',
-//     Year: '2015',
-//     imdbID: 'tt1817771',
-//     Type: 'movie',
-//     Poster: 'https://m.media-amazon.com/images/M/MV5BMTk2MTMyMTEzN15BMl5BanBnXkFtZTgwNTE0MDYwNzE@._V1_SX300.jpg'
-//   },
-//   {
-//     Title: 'Force of Nature',
-//     Year: '2020',
-//     imdbID: 'tt10308928',
-//     Type: 'movie',
-//     Poster: 'https://m.media-amazon.com/images/M/MV5BNjZjODI5NmUtZTM3ZS00ZDkyLWJiMDYtYjI4MzFhODg1MDFmXkEyXkFqcGdeQXVyOTg4MDYyNw@@._V1_SX300.jpg'
-//   },
-//   {
-//     Title: 'The Nut Job 2: Nutty by Nature',
-//     Year: '2017',
-//     imdbID: 'tt3486626',
-//     Type: 'movie',
-//     Poster: 'https://m.media-amazon.com/images/M/MV5BZTA5ZDZhMjgtNjNkOC00MzM0LWFlZmItZTM4MjJkMGEyZGQwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_SX300.jpg'
-//   },
-//   {
-//     Title: 'Scenes of a Sexual Nature',
-//     Year: '2006',
-//     imdbID: 'tt0475380',
-//     Type: 'movie',
-//     Poster: 'https://m.media-amazon.com/images/M/MV5BMTA5OTI0MTgwNTheQTJeQWpwZ15BbWU3MDc1MzUyNTE@._V1_SX300.jpg'
-//   },
-//   {
-//     Title: 'Evil Nature',
-//     Year: '2018',
-//     imdbID: 'tt4624768',
-//     Type: 'movie',
-//     Poster: 'https://m.media-amazon.com/images/M/MV5BYTA2NzBkZGUtOTgyNi00ODUyLTliZTQtNjBhMTNlZWEzZTI3XkEyXkFqcGdeQXVyMjc5MDM3Mw@@._V1_SX300.jpg'
-//   },
-//   {
-//     Title: 'Nature Calls',
-//     Year: '2012',
-//     imdbID: 'tt1493157',
-//     Type: 'movie',
-//     Poster: 'https://m.media-amazon.com/images/M/MV5BMTM1NDE0NTI5OF5BMl5BanBnXkFtZTcwOTY1MTY0OA@@._V1_SX300.jpg'
-//   },
-//   {
-//     Title: 'Out of Nature',
-//     Year: '2014',
-//     imdbID: 'tt3596492',
-//     Type: 'movie',
-//     Poster: 'https://m.media-amazon.com/images/M/MV5BMzQzNzdhZTctYTQzNi00ZGE0LThhMzAtODU2Njk0MzhkOGU3XkEyXkFqcGdeQXVyMjA0MzYwMDY@._V1_SX300.jpg'
-//   }
