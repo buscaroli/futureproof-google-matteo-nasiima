9cee366a1e44e6e2bcdb2a677c40671bec15f12c
mabus <m.buscaroli@outlook.com>
2022-05-12 12:32:39 +0100

    done some live testing of the frontend with nasiima

--- client/tests/client.test.js
+++ client/tests/client.test.js
@@ -6,15 +6,31 @@ const fs = require('fs')
 const path = require('path')
 const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8')
 
+// describe is like a comment where you describe what you are going
+// to test inside of it
 describe('client/index.html', () => {
   // before each test is run we want to start with a brand new DOM
+  // every time this file is run by jest we get a brand new Page
   beforeEach(() => {
     document.documentElement.innerHTML = html.toString()
   })
 
+  // Then you do all the tests with either 'it' ot 'test'
   it('tests the navbar exists', () => {
     let navbar = document.querySelector('.navbar')
     expect(navbar).toBeTruthy()
+  })
+
+  it('tests the first link with an id of link-1 has a text of Gmail', () => {
+    const link1 = document.querySelector('#link-1')
+
+    // toMatch is very good for Strings because you can use RegExp (/string in here /)
+    expect(link1.textContent).toMatch(/Gmail/)
+  })
+
+  it('tests that the lik with an id of link-2 has a text of Images', () => {
+    const link2 = document.querySelector('#link-2')
+    expect(link2.textContent).toEqual('Images')
   })
 
   it('tests the searchbar exists', () => {
